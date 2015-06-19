4152573fea2d793c38860c1f3b95af00f90de5db
jessen <jeschou.zsy@gmail.com>
2015-06-19 15:18:41 +0800

    增加日志输出

--- aurora/bpm/engine/ExecutorContext.java
+++ aurora/bpm/engine/ExecutorContext.java
@@ -1,19 +1,31 @@
 package aurora.bpm.engine;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mysql.jdbc.Connection;
+
 import uncertain.composite.CompositeMap;
 import uncertain.ocm.IObjectRegistry;
 import aurora.bpm.command.CommandRegistry;
+import aurora.bpm.command.sqlje.ProcessLogProc;
 import aurora.bpm.model.DefinitionFactory;
 import aurora.bpm.script.BPMScriptEngine;
+import aurora.database.service.IDatabaseServiceFactory;
 import aurora.plugin.script.engine.AuroraScriptEngine;
 import aurora.plugin.script.scriptobject.ScriptShareObject;
 import aurora.sqlje.core.IInstanceManager;
+import aurora.sqlje.core.ISqlCallStack;
+import aurora.sqlje.core.SqlCallStack;
 
 public class ExecutorContext {
 	private DefinitionFactory factory;
 	private IInstanceManager instManger;
 	private CommandRegistry cmdRegistry;
 	private IObjectRegistry ior;
+	private ProcessLogProc logger;
+	private SqlCallStack loggerCallStack;
 
 	public ExecutorContext(IInstanceManager instManger,
 			DefinitionFactory defFactory, IObjectRegistry ior) {
@@ -21,13 +33,38 @@ public class ExecutorContext {
 		this.instManger = instManger;
 		factory = defFactory;
 		this.ior = ior;
+		try {
+			createLogger();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 		System.out.println(getClass().getSimpleName() + " instance created.");
 	}
 
-	public void destory() {
-		
+	private void createLogger() throws SQLException {
+		IDatabaseServiceFactory dsf = (IDatabaseServiceFactory) ior
+				.getInstanceOfType(IDatabaseServiceFactory.class);
+		DataSource ds = dsf.getDataSource();
+		java.sql.Connection conn = ds.getConnection();
+		conn.setAutoCommit(false);
+		loggerCallStack = new SqlCallStack(ds, conn);
+		loggerCallStack.setContextData(new CompositeMap("context"));
+		logger = instManger.createInstance(ProcessLogProc.class);
+		logger._$setSqlCallStack(loggerCallStack);
 	}
 
+	public ProcessLogProc getLogger() {
+		return logger;
+	}
+
+	public void destory() {
+		try {
+			loggerCallStack.cleanUp();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public IObjectRegistry getObjectRegistry() {
 		return ior;
